f058a12f1dc5a84cef3379ff60c178b5c767cc23
wonderotter <gouk32@naver.com>
2019-08-11 02:42:25 +0900

    Wrapping a REST API with GraphQL

--- graphql/db.js
+++ graphql/db.js
@@ -1,58 +1,21 @@
-let movies = [
-    {   id: 1,
-        name: "Zootopia",
-        score: 0.92
-    },
-    {   id: 2,
-        name: "How to train your dragon",
-        score: 0.91
-    },
-    {
-        id: 3,
-        name: "SPIDER-MAN: FAR FROM HOME",
-        score: 0.95
-    },
-    {
-        id: 4,
-        name: "ALADIN",
-        score: 0.94
-    },
-    {
-        id: 5,
-        name: "YESTERDAY",
-        score: 0.89
+import fetch from 'node-fetch';
+
+const API_URL = "https://yts.lt/api/v2/list_movies.json?";
+
+export const getMovies = (genre, rating) => {
+    let REQUEST_URL = API_URL;
+
+    if(genre !== undefined || genre !== null){
+        REQUEST_URL += `genre=${genre}`;
     }
-];
 
-export const getMovies = () => movies;
+    if(rating > 0){
+        REQUEST_URL += `&minimum_rating=${rating}`;
+    }
 
-export const getById = id => {
-    const filteredMovie = movies.filter(movie => movie.id === id);
-
-    return filteredMovie[0];
+    const data = fetch(REQUEST_URL)
+                .then(res => res.json())
+                .then(json => json.data.movies);
+                
+    return data;
 };
-
-export const deleteMovie = id => {
-    const cleanedMovies = movies.filter(movie => movie.id !== id);
-
-    if(movies.length > cleanedMovies.length){
-        movies = cleanedMovies;
-        
-        return true;
-    }else{
-    
-        return false;
-    }
-}
-
-export const addMovie = (name, score) => {
-    const newMovie = {
-        id: parseInt(`${movies.length + 1}`),
-        name: name,
-        score: score
-    };
-
-    movies.push(newMovie);
-
-    return newMovie;
-}
