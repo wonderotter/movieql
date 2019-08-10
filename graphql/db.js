669662c8d1056d8e2bb919636e605e3e54ce3f30
wonderotter <gouk32@naver.com>
2019-08-11 03:24:16 +0900

    axios로 바꾸기 & movie, suggestions 추가

--- graphql/db.js
+++ graphql/db.js
@@ -1,21 +1,39 @@
-import fetch from 'node-fetch';
+import axios from 'axios';
 
-const API_URL = "https://yts.lt/api/v2/list_movies.json?";
+const BASE_URL = "https://yts.lt/api/v2/";
+const LIST_MOVIES_URL = `${BASE_URL}list_movies.json`;
+const MOVIE_DETAILS_URL = `${BASE_URL}movie_details.json`;
+const MOVIE_SUGGESTIONS_URL = `${BASE_URL}movie_suggestions.json`;
 
-export const getMovies = (genre, rating) => {
-    let REQUEST_URL = API_URL;
+export const getMovies = async (genre, limit, rating) => {
+    const {
+        data: {
+            data: { movies }
+        }
+    } = await axios(LIST_MOVIES_URL, {
+        params: {
+            genre,
+            limit,
+            minimum_rating: rating
+        }
+    });
+    return movies;
+};
 
-    if(genre !== undefined || genre !== null){
-        REQUEST_URL += `genre=${genre}`;
-    }
+export const getMovie = async id => {
+    const { data: { data: { movie } } } = await axios(MOVIE_DETAILS_URL,{
+        params: {
+            movie_id: id
+        }
+    });
+    return movie;
+};
 
-    if(rating > 0){
-        REQUEST_URL += `&minimum_rating=${rating}`;
-    }
-
-    const data = fetch(REQUEST_URL)
-                .then(res => res.json())
-                .then(json => json.data.movies);
-                
-    return data;
+export const getSuggestions = async id => {
+    const {data: {data: { movies } } } = await axios(MOVIE_SUGGESTIONS_URL,{
+        params: {
+            movie_id: id
+        }
+    });
+    return movies;
 };
